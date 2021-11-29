5d050d5d374fed3698af7315adff694afb9d731d
Sergey Maximov <dev9092336062@gmail.com>
2021-11-29 09:47:10 +0300

    вынесен стиль кнопки

--- src/js/home.js
+++ src/js/home.js
@@ -1,49 +1,27 @@
 
-// Page Home
+// click homeListBtn" - "homeListCode" : height: 'auto'; padding: '20px';
+// hover homeListBtn" - "homeListCode" : box-shadow: '$shadow-hover__dark';
+
 // batton code
 
 const codes = document.querySelectorAll('.homeListCode');
 const buttonsArr = document.querySelectorAll('.homeListBtn');
+const buttonsOnn = new Map()
+const buttonsOff = new Map()
 
-let buttonsOnn = new Map()
-let buttonsOff = new Map()
+arrInMap(buttonsArr, buttonsOnn)
 
-function addButtonsOnn(){
-  for(let i = 0; i < buttonsArr.length; i++){
-    buttonsOnn.set(i,buttonsArr[i])
-  }
-}
-
-function homeCodeOnnbtn() {
-  const buttonsOnnKeys = buttonsOnn.keys()
-  for (let KeyOnn of buttonsOnnKeys) {
-    const buttonOnn = buttonsOnn.get(KeyOnn)
-    buttonOnn.addEventListener('click', () => {
-      codes[KeyOnn].classList.add('homeListCodeActive')
-      buttonOnn.innerHTML = "Скрыть JS код"
-      buttonsOff.set(KeyOnn, buttonOnn)
-      buttonsOnn.delete(KeyOnn)
-    })
-  }
-}
-
-function homeCodeOffbtn() {
-  const buttonsOffkeys = buttonsOff.keys()
-  for (let keyOff of buttonsOffkeys) {
-    const buttonOff = buttonsOff.get(keyOff)
-    buttonOff.addEventListener('click', () => {
-      codes[keyOff].classList.remove('homeListCodeActive')
-      buttonOff.innerHTML = "Открыть JS код"
-      buttonsOnn.set(keyOff, buttonOff)
-      buttonsOff.delete(keyOff)
-    })
-  }
+function buttonCode(){
+    
 }
 
 
-addButtonsOnn()
-homeCodeOnnbtn()
-homeCodeOffbtn()
 
+console.log(buttonsOnn)
+console.log(buttonsOff)
 
+// функция заполнения map кнопок из массива
+function arrInMap(arr, map){
+    for(let i = 0; i < arr.length;i++) map.set(i, arr[i])
+}
 
