5d7474ef3a75dda44b9e2e37f9e9a2711a1bec28
Sergey Maximov <dev9092336062@gmail.com>
2021-11-29 10:17:26 +0300

    добавлен скрипт наведения на кнопку открытия кода

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,7 @@
 // click homeListBtn" - "homeListCode" : height: 'auto'; padding: '20px';
 // hover homeListBtn" - "homeListCode" : box-shadow: '$shadow-hover__dark';
 
-// batton code
+// button code
 
 const codes = document.querySelectorAll('.homeListCode');
 const buttonsArr = document.querySelectorAll('.homeListBtn');
@@ -11,17 +11,22 @@ const buttonsOff = new Map()
 
 arrInMap(buttonsArr, buttonsOnn)
 
-function buttonCode(){
-    
+function buttonHover(){
+    for(let i = 0; i < buttonsArr.length; i++){
+        buttonsArr[i].addEventListener ('mouseover', ()=>{
+            codes[i].style.transition = "all 0.3s"
+            codes[i].style.boxShadow = "0 0 20px #663300"
+        })
+        buttonsArr[i].addEventListener ('mouseout', ()=>{
+            codes[i].style.boxShadow = "0 0 0px #663300"
+        })
+    }
+
 }
 
-
-
-console.log(buttonsOnn)
-console.log(buttonsOff)
-
-// функция заполнения map кнопок из массива
+// функция заполнения map из массива
 function arrInMap(arr, map){
     for(let i = 0; i < arr.length;i++) map.set(i, arr[i])
 }
 
+buttonHover()
