2324b10594e2042ab1bf541707892bd48b29bdcf
Sergey Maximov <dev9092336062@gmail.com>
2021-12-02 11:29:45 +0300

    fixed js for project_6

--- src/js/project_6.js
+++ src/js/project_6.js
@@ -1,70 +1,6 @@
 
-const fieldRows = [...document.querySelectorAll('.p6FieldRow')]
+//_____create a Field
 const field = []
-
-class Turtle {
-  constructor(name, x = 0, y = 0) {
-    this.name = name
-    this.position = { x, y }
-  }
-  move(...placements) {
-    for (let placement of placements) {
-      for (let onePlacement of placement.split(' ')) {
-        if (onePlacement == 'right') {
-          if (this.position.x < 4) {
-            clearTurtleStyle()
-            field[this.position.y][this.position.x + 1].classList.add('p6TurtleStyle')
-            this.position.x++
-          }
-        }
-        if (onePlacement == 'left') {
-          if (this.position.x > 0) {
-            clearTurtleStyle()
-            field[this.position.y][this.position.x - 1].classList.add('p6TurtleStyle')
-            this.position.x--
-          }
-        }
-        if (onePlacement == 'up') {
-          if (this.position.y > 0) {
-            clearTurtleStyle()
-            field[this.position.y - 1][this.position.x].classList.add('p6TurtleStyle')
-            this.position.y--
-          }
-        }
-        if (onePlacement == 'down') {
-          if (this.position.y < 4) {
-            clearTurtleStyle()
-            field[this.position.y + 1][this.position.x].classList.add('p6TurtleStyle')
-            this.position.y++
-          }
-        }
-      }
-    }
-    return this
-  }
-
-  showPosition() {
-    console.log(`(${this.position.x}, ${this.position.y})`)
-    return this
-  }
-}
-
-const SuperTurtle = new Turtle('SuperTurtle')
-
-const buttonUp = document.querySelector('.p6BtnUp')
-const buttonDown = document.querySelector('.p6BtnDown')
-const buttonLeft = document.querySelector('.p6BtnLeft')
-const buttonRight = document.querySelector('.p6BtnRight')
-const moveUp = buttonUp.addEventListener('click', () => SuperTurtle.move('up'))
-const moveDown = buttonDown.addEventListener('click', () => SuperTurtle.move('down'))
-const moveLeft = buttonLeft.addEventListener('click', () => SuperTurtle.move('left'))
-const moveRight = buttonRight.addEventListener('click', () => SuperTurtle.move('right'))
-const buttonsClick = [moveUp, moveDown, moveLeft, moveRight]
-
-function clearTurtleStyle() {
-  const elements = document.querySelectorAll('.p6FieldElement')
-  for (let el of elements) el.classList.remove('p6TurtleStyle')
-}
 
 function createField(arr) {
   for (let i = 0; i < 5; i++) {
@@ -72,8 +8,69 @@ function createField(arr) {
   }
 }
 
-createField(fieldRows)
+createField([...document.querySelectorAll('.p6FieldRow')])
 
-field[SuperTurtle.position.y][SuperTurtle.position.x].classList.add('p6TurtleStyle')
+//_____Turtle
+class Turtle {
+  constructor(name, x = 0, y = 0) {
+    this.name = name
+    this.position = { x, y }
+  }
+  //_method: movement Turtle
+  move(...placements) {
+    for (let placement of placements) {
+      for (let onePlacement of placement.split(' ')) {
+        //_clear the style of the turtle's previous position
+        field[this.position.y][this.position.x].classList.remove('p6TurtleStyle')
+        //_
+        if (onePlacement == 'right') {
+          if (this.position.x < 4) {
+            this.position.x++
+          }
+        }
+        if (onePlacement == 'left') {
+          if (this.position.x > 0) {
+            this.position.x--
+          }
+        }
+        if (onePlacement == 'up') {
+          if (this.position.y > 0) {
+            this.position.y--
+          }
+        }
+        if (onePlacement == 'down') {
+          if (this.position.y < 4) {
+            this.position.y++
+          }
+        }
+        //_assign style and show message on console
+        field[this.position.y][this.position.x].classList.add('p6TurtleStyle')
+        console.log(`// Turtle ${this.name} moved ${onePlacement} (${this.position.x}, ${this.position.y})`)
+      }
+    }
+    return this
+  }
 
-console.log('132')
+  //_method: display the current position of the turtle
+  showPosition() {
+    console.log(`(${this.position.x}, ${this.position.y})`)
+    return this
+  }
+}
+
+//_____Turtle which is displayed in the browser
+const VTurtle = new Turtle('VTurtle')
+
+//___put the turtle on the starting element
+field[VTurtle.position.y][VTurtle.position.x].classList.add('p6TurtleStyle')
+
+//_____browser button listeners
+const buttonUp = document.querySelector('.p6BtnUp')
+const buttonDown = document.querySelector('.p6BtnDown')
+const buttonLeft = document.querySelector('.p6BtnLeft')
+const buttonRight = document.querySelector('.p6BtnRight')
+const moveUp = buttonUp.addEventListener('click', () => VTurtle.move('up'))
+const moveDown = buttonDown.addEventListener('click', () => VTurtle.move('down'))
+const moveLeft = buttonLeft.addEventListener('click', () => VTurtle.move('left'))
+const moveRight = buttonRight.addEventListener('click', () => VTurtle.move('right'))
+
